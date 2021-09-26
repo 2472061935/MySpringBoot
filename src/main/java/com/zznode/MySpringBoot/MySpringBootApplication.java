ab69f243063dd67eb7512b135f06e46a4b7303a9
12133 <12133@LAPTOP-3T8G8CBO>
2021-09-26 17:57:50 +0800

    Pom.xml文件提交

--- src/main/java/com/zznode/MySpringBoot/MySpringBootApplication.java
+++ src/main/java/com/zznode/MySpringBoot/MySpringBootApplication.java
@@ -1,9 +1,11 @@
 package com.zznode.MySpringBoot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.zznode.*.Mapper")
 public class MySpringBootApplication {
 
 	public static void main(String[] args) {
